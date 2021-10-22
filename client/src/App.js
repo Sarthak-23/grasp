8c9440e7a4fcc27ec843dba39893bab3854abd94
Sarthak-23 <jainsarthak0786@gmail.com>
2021-10-22 22:03:39 +0530

    Set the Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,24 @@
 import React from "react";
+import { Route, Switch } from 'react-router-dom';
 
 import classes from './App.css';
 
 function App() {
   return (
-    <div className={classes.App}>
-      
-    </div>
+    <Switch>
+      <Route exact path="/login">
+          {/* Login Component */}
+      </Route>
+      <Route exact path="/register">
+          {/* Register Component */}
+      </Route>
+      <Route path="/">
+          {/* Home Component */}
+      </Route>
+      <Route exact path="/search">
+          {/* Search Component */}
+      </Route>
+    </Switch>
   );
 }
 
