1912ea38341dfdaba07fbac71fc6eec93e3f454e
Aditya <adijr9487@gmail.com>
2021-10-22 20:55:49 +0530

    NPM EJECT CSS MODULE

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 require('./config/dbConfig').config();
 const express = require('express');
 const homeRoutes = require('./routes/home');
+const cors = require("cors")
+
 
 // Important constants
 const app = express();
@@ -9,6 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middle wares
 app.use(express.json());
+app.use(cors());
 
 // Routing
 app.use('/', homeRoutes);
