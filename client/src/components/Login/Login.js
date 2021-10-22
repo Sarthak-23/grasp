5047af909373f89d76ba4999300289c19069d72f
Divyans Falodiya <divyanshfalodiyaab@gmail.com>
2021-10-23 00:19:51 +0530

    :art: changed layout

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import Grid from '@mui/material/Grid'
+import React from 'react';
+import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Avatar from '@mui/material/Avatar';
 import TextField from '@mui/material/TextField';
@@ -8,36 +8,84 @@ import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
 import Checkbox from '@mui/material/Link';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import { makeStyles } from '@mui/styles';
+import classes from './Login.css';
 
-//component
-
-//classes
-import classes from "./Login.css"
+const useStyles = makeStyles((theme) => ({
+    container: {
+        // width: '100vw',
+        height: '100vh',
+        margin: '0',
+        display: 'flex',
+        alignItems: 'center',
+        // border: `1px solid ${theme.palette.divider}`,
+        // borderRadius: '1rem',
+    },
+    form: {
+        padding: 30,
+        margin: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    lock: {},
+}));
 
 const Login = () => {
-    const paperStyle={padding :20,height:'50vh',width:300, margin:"20px auto"}
-    const avatarStyle={backgroundColor:'#1bbd7e'}
-    const btnstyle={margin:'8px 0'}
-    return(
-        <Grid style={{marginTop:"80px"}} align='center'>
-            <Paper elevation={20} style={paperStyle}>
-                <Grid align='center'>
-                     <Avatar style={avatarStyle}>
-                        <LockOutlinedIcon color="blue"/>
-                     </Avatar>
+    const classes = useStyles();
+    const avatarStyle = { backgroundColor: '#1bbd7e', margin: 'auto 0.5rem' };
+    const btnstyle = { margin: '8px 0' };
+    return (
+        <Grid className={classes.container}>
+            <Paper elevation={20} className={classes.form}>
+                <div
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Avatar style={avatarStyle} className={classes.lock}>
+                        <LockOutlinedIcon />
+                    </Avatar>
                     <h2>Log In</h2>
-                </Grid>
-                <TextField label='Username' variant="outlined" placeholder='Enter username' fullWidth required style={{marginTop:"20px"}}/>
-                <TextField label='Password' placeholder='Enter password' type='password' fullWidth required style={{marginTop:"20px"}}/>
-                <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                </div>
+                <TextField
+                    label="Username"
+                    variant="outlined"
+                    placeholder="Enter username"
+                    fullWidth
+                    required
+                    style={{ marginTop: '20px' }}
+                />
+                <TextField
+                    label="Password"
+                    placeholder="Enter password"
+                    type="password"
+                    fullWidth
+                    required
+                    style={{ marginTop: '20px' }}
+                />
+                <Button
+                    type="submit"
+                    color="primary"
+                    variant="contained"
+                    style={btnstyle}
+                    fullWidth
+                >
+                    Sign in
+                </Button>
                 <Typography>
-                     <Link href="#" style={{textDecoration:"none", color:"black"}} >
-                     Do you have an account? 
-                </Link>
+                    <Link
+                        href="#"
+                        style={{ textDecoration: 'none', color: 'black' }}
+                    >
+                        Do you have an account?
+                    </Link>
                 </Typography>
             </Paper>
         </Grid>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
