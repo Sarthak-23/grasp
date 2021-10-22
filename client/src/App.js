2f483b63269967b3566c4824199c2824741d1b16
Aditya <adijr9487@gmail.com>
2021-10-22 22:04:49 +0530

    panel

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 import classes from './App.css';
+import Panel from "./Component/Panel/Panel";
 
 function App() {
   return (
     <div className={classes.App}>
-      
+      <Panel />
     </div>
   );
 }
---
+++ client/src/Component/Panel/Panel.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+//componennt
+
+//classes
+import classes from "./Panel.css";
+
+const Panel = (props) => {
+    return (
+        <div className={classes.Panel}>
+            hello
+        </div>
+    )
+}
+
+export default Panel
