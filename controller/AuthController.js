50cd14c690f52787a14e1ec7c0e059e2312e8a30
Divyans Falodiya <divyanshfalodiyaab@gmail.com>
2021-10-22 23:45:21 +0530

    :passport_control: setup passport

--- controller/AuthController.js
+++ controller/AuthController.js
@@ -1,5 +1,67 @@
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const User = require('../models/User');
+const saltrounds = 10;
 
-exports.login = (req, res) => {};
+// Serialize user
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+});
 
-exports.register = (req, res) => {};
+// Deserialize user
+passport.deserializeUser(async (id, done) => {
+    try {
+        const user = await User.findById(id);
+        if (user) {
+            done(err, user);
+        }
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+// Login Strategy
+passport.use(
+    'login',
+    new LocalStrategy(async (req, username, password, done) => {
+        try {
+            const user = await User.findOne({ username: username });
+            if (!user) {
+                return done(null, false, { message: 'Invalid credentials!' });
+            }
+            const result = await bcrypt.compare(password, user.password);
+            if (!result) {
+                return done(null, false, { message: 'Invalid credentials!' });
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
+
+// Register Strategy
+passport.use(
+    'register',
+    new LocalStrategy(async (req, username, password, done) => {
+        try {
+            let user = await User.findOne({ username: username });
+            if (user) {
+                return done(null, false, {
+                    message: 'Username already exists',
+                });
+            }
+            const hashed = await bcrypt.hash(password, saltrounds);
+            user = User({
+                username: username,
+                password: hashed,
+                name: req.body.name,
+            });
+            await user.save();
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    })
+);
