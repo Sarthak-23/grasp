066dad9fb54a8d378afbc172469010c44e256bc0
Sarthak-23 <jainsarthak0786@gmail.com>
2021-10-23 19:28:25 +0530

    Right Panel Updated

--- client/src/components/Home/RightPanel/RightPanel.js
+++ client/src/components/Home/RightPanel/RightPanel.js
@@ -1,12 +1,7 @@
 import React, { useState } from 'react';
-import Icon from '@mui/material/Icon';
-import BorderColorIcon from '@mui/icons-material/BorderColor';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Modal from '@mui/material/Modal';
 import classes from './RightPanel.css';
-import Backdrop from '@mui/material/Backdrop';
-import TextField from '@mui/material/TextField';
+import TextareaAutosize from '@mui/material/TextareaAutosize';
+import Button from '@mui/material/Button';
 
 const style = {
     position: 'absolute',
@@ -21,63 +16,33 @@ const style = {
 };
 
 const RightPanel = () => {
-    const [open, setOpen] = React.useState(false);
-    const [description, setDescription] = useState('');
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
+    const [des_disable, setDesdisable] = useState(true);
+    const [mat_disable, setMatdisable] = useState(true);
+
     return (
         <div className={classes.Container}>
             <h1>Topic Name</h1>
-            <div className={classes.Description}>
-                <BorderColorIcon
-                    className={classes.Edit}
-                    onClick={handleOpen}
-                />
-                <br />
-                <br />
-                <div style={{ textAlign: 'center' }}>
-                    <p>Description</p>
-                    <br />
-                </div>
-            </div>
-            <div className={classes.Materials}>
-                <p>Materials</p>
-            </div>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="modal-modal-title"
-                aria-describedby="modal-modal-description"
-                closeAfterTransition
-                BackdropComponent={Backdrop}
-                BackdropProps={{
-                    timeout: 500,
-                }}
+            <TextareaAutosize
+                aria-label="minimum height"
+                minRows={10}
+                placeholder="Description"
+                className={classes.textArea}
+                disabled={des_disable}
+            />
+            <TextareaAutosize
+                aria-label="minimum height"
+                minRows={5}
+                placeholder="Materials"
+                className={classes.textArea}
+                disabled={mat_disable}
+            />
+            <Button
+                variant="contained"
+                color="success"
+                className={classes.Button}
             >
-                <Box sx={style}>
-                    <Typography
-                        id="modal-modal-title"
-                        variant="h6"
-                        component="h2"
-                    >
-                        Topic
-                    </Typography>
-                    <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-                        <TextField
-                            label="Description"
-                            variant="outlined"
-                            placeholder="Enter Description"
-                            fullWidth
-                            required
-                            style={{ marginTop: '20px' }}
-                            name="description"
-                            id="description"
-                            value={description}
-                            onChange={(e) => setDescription(e.target.value)}
-                        />
-                    </Typography>
-                </Box>
-            </Modal>
+                Update
+            </Button>
         </div>
     );
 };
