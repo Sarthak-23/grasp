a8266f6a15e6b545eca8e68bbf8d91f673511d0e
Sarthak-23 <jainsarthak0786@gmail.com>
2021-10-23 22:19:14 +0530

    Fetch the User profile in Profile component

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,6 +1,34 @@
 import { Box } from '@mui/system';
+import React, { useState } from 'react';
+import { useParams } from 'react-router';
 
 const Profile = (props) => {
+    const [profile, setProfile] = useState({});
+    const { username } = useParams();
+    const fetchProfile = async () => {
+        try {
+            const res = await fetch(`/profile/${username}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            const data = await res.json();
+            if (data.error) throw data.error;
+            return data;
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    useEffect(() => {
+        fetchProfile().then((res) => {
+            if (res) {
+                setProfile(res);
+            }
+        });
+    }, []);
+
     return <Box></Box>;
 };
 
