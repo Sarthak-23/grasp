244a4cd02352102c272a37096a79ee1f70a4eae9
Divyans Falodiya <divyanshfalodiyaab@gmail.com>
2021-10-23 17:09:20 +0530

    :sparkles: added user context

--- client/src/App.js
+++ client/src/App.js
@@ -4,26 +4,29 @@ import Login from '../src/components/Login/Login';
 import Register from '../src/components/Register/Register';
 import Home from '../src/components/Home/Home';
 import Search from '../src/components/Search/Search';
+import { UserProvider } from './context/UserContext';
 
 //classes
 import classes from './App.css';
 
 function App() {
     return (
-        <Switch>
-            <Route exact path="/login">
-                <Login />
-            </Route>
-            <Route exact path="/register">
-                <Register />
-            </Route>
-            <Route exact path="/">
-                <Home />
-            </Route>
-            <Route exact path="/search">
-                <Search />
-            </Route>
-        </Switch>
+        <UserProvider>
+            <Switch>
+                <Route exact path="/login">
+                    <Login />
+                </Route>
+                <Route exact path="/register">
+                    <Register />
+                </Route>
+                <Route exact path="/">
+                    <Home />
+                </Route>
+                <Route exact path="/search">
+                    <Search />
+                </Route>
+            </Switch>
+        </UserProvider>
     );
 }
 
---
+++ client/src/context/UserContext.js
@@ -0,0 +1,17 @@
+import React, { useState, createContext } from 'react';
+
+export const UserContext = createContext();
+
+export const UserProvider = (props) => {
+    const [user, setUser] = useState({
+        id: '',
+        username: '',
+        name: '',
+    });
+
+    return (
+        <UserContext.Provider value={[user, setUser]}>
+            {props.children}
+        </UserContext.Provider>
+    );
+};
