03d4f47ef88a763bddec699e5a5ac5c9e8e998fd
Sarthak-23 <jainsarthak0786@gmail.com>
2021-10-24 17:00:30 +0530

    Implement the pending, accept and decline status in userconnection component

--- client/src/components/UserList/UserConnectionRequestItem.js
+++ client/src/components/UserList/UserConnectionRequestItem.js
@@ -1,8 +1,49 @@
 import { Avatar, ListItem, ListItemIcon, ListItemText } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Chip from '@mui/material/Chip';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import Icon from '@mui/material/Icon';
+import Tooltip from '@mui/material/Tooltip';
 
 const UserConnectionRequestItem = (props) => {
-    const { index, user } = props;
+    const { index, user, type } = props;
+
+    const handleAccept = async (e) => {
+        e.preventDefault();
+        try {
+            let res = await fetch(`/${user.username}/accept`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(user.username),
+            });
+            res = await res.json();
+
+            //Rest will done by backend developer (On Current Project)
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const handleDecline = async (e) => {
+        e.preventDefault();
+        try {
+            let res = await fetch(`/${user.username}/reject`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(user.username),
+            });
+            res = await res.json();
+
+            // Rest will done by backend developer (On Current Project)
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
     return (
         <Link
             key={index}
@@ -14,11 +55,36 @@ const UserConnectionRequestItem = (props) => {
                     <Avatar />
                 </ListItemIcon>
                 <ListItemText primary={user.username} secondary={user.name} />
-                <ListItemText
-                    secondary={
-                        user.about ? user.about.slice(0, 100) + '...' : ''
-                    }
-                />
+                {type === 'Pending' ? (
+                    <Chip
+                        key={index}
+                        label={'Pending'}
+                        size="small"
+                        style={{
+                            backgroundColor: '#90ee90',
+                            color: 'white',
+                        }}
+                    />
+                ) : (
+                    <>
+                        <Tooltip title="Accept">
+                            <Icon
+                                style={{ color: 'green' }}
+                                onClick={handleAccept}
+                            >
+                                check_circle
+                            </Icon>
+                        </Tooltip>
+                        <Tooltip title="Decline">
+                            <Icon
+                                style={{ color: 'red' }}
+                                onClick={handleDecline}
+                            >
+                                highlight_off
+                            </Icon>
+                        </Tooltip>
+                    </>
+                )}
             </ListItem>
             <hr />
         </Link>
